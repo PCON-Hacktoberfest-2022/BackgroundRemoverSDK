a60383623c84ba5b7c0c55fbb15f80f4d64d50ab
Abhinav Singh <abhirishi0805@gmail.com>
2022-10-20 18:49:48 +0530

    Update Constants.kt

--- app/src/main/java/com/ersubhadip/removebg/Models/Constants.kt
+++ app/src/main/java/com/ersubhadip/removebg/Models/Constants.kt
@@ -4,6 +4,6 @@ class Constants {
 
     companion object{
         const val BASE_URL = "https://background-removal.p.rapidapi.com"
-        const val KEY = "336579e4ecmshec192c5680adc62p1da429jsn5730cec6b742"   //temporary
+        const val KEY = "336579e4ecmshec192c5680adc62p1da429jsn5730cec6b742"   // temporary
     }
 }
